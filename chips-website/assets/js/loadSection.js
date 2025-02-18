ba9b346d00ba1865b9bfdb21dd59d801bd11add5
alXorazmiy <alxorazmiy1997@gmail.com>
2025-02-18 12:18:54 +0500

    commit

--- chips-website/assets/js/loadSection.js
+++ chips-website/assets/js/loadSection.js
@@ -5,6 +5,22 @@ document.addEventListener("DOMContentLoaded", function () {
         .then(response => response.text())
         .then(data => {
             document.getElementById("home").innerHTML = data;
+
+                const sr = ScrollReveal({
+                    origin :'top',
+                    distance : '60px',
+                    duration: 2500,
+                    delay: 300,
+                    reset: true
+                })
+
+
+                sr.reveal(".home__data")
+                sr.reveal(".home__circle, .home__img", {delay: 600, scale: .5})
+                sr.reveal(".home__chips-1, .home__chips-2, .home__chips-3", {delay: 1000, interval: 100})
+                sr.reveal(".home__leaf", {delay: 1200})
+                sr.reveal(".home__tomato-1, .home__tomato-2", {delay: 1400, interval: 100})
+
         })
         .catch(error => console.error("Home section yuklashda xatolik:", error));
 });
@@ -16,6 +32,16 @@ document.addEventListener("DOMContentLoaded", function () {
         .then(response => response.text())
         .then(data => {
             document.getElementById("care").innerHTML = data;
+            const sr = ScrollReveal({
+                origin :'top',
+                distance : '60px',
+                duration: 2500,
+                delay: 300,
+                reset: true
+
+            })
+            sr.reveal(".care__img", {origin: "left"})
+            sr.reveal(".care__list", {origin: "right"})
         })
         .catch(error => console.error("Home section yuklashda xatolik:", error));
 });
@@ -24,6 +50,17 @@ document.addEventListener("DOMContentLoaded", function () {
         .then(response => response.text())
         .then(data => {
             document.getElementById("products").innerHTML = data;
+            const sr = ScrollReveal({
+                origin :'top',
+                distance : '60px',
+                duration: 2500,
+                delay: 300,
+                reset: true
+
+            })
+
+
+            sr.reveal(".products__card", {interval: 100})
         })
         .catch(error => console.error("Home section yuklashda xatolik:", error));
 });
@@ -33,7 +70,17 @@ document.addEventListener("DOMContentLoaded", function () {
         .then(response => response.text())
         .then(data => {
             document.getElementById("favorites").innerHTML = data;
+            const sr = ScrollReveal({
+                origin :'top',
+                distance : '60px',
+                duration: 2500,
+                delay: 300,
+                reset: true
 
+            })
+
+
+            sr.reveal(".favorites__container")
             const swiperFavorites = new Swiper(".favorites__swiper", {
                 loop: true,
                 grabCursor: true,
